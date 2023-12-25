c29531a1452e3f21dd1e73d65e52b4bb78562239
fuliang <fuliang19890809@163.com>
2023-12-25 11:04:07 +0800

    优化单元测试

--- client_test.go
+++ client_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/go-redis/redis/v8"
 	"github.com/stretchr/testify/assert"
+	"sync"
 	"testing"
 	"time"
 )
@@ -18,19 +19,40 @@ var lockKey = "test"
 
 func TestLock_success(t *testing.T) {
 	client := Instance(context.Background(), redis.NewClient(opt))
-	locked := client.Lock(lockKey, 10*time.Second)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	var locked bool
+	go func() {
+		defer wg.Done()
+		locked = client.Lock(lockKey, 10*time.Second)
+		client.UnLock(lockKey)
+	}()
+	go func() {
+		defer wg.Done()
+		locked = client.Lock(lockKey, 10*time.Second)
+		client.UnLock(lockKey)
+	}()
+	wg.Wait()
 	assert.True(t, locked)
-	client.UnLock(lockKey)
-	locked = client.Lock(lockKey, 10*time.Second)
-	assert.True(t, locked)
-	client.UnLock(lockKey)
 }
 
 func TestLock_fail(t *testing.T) {
 	client := Instance(context.Background(), redis.NewClient(opt))
-	locked := client.Lock(lockKey, 10*time.Second)
-	assert.True(t, locked)
-	unLock := client.Lock(lockKey, 10*time.Second)
-	assert.False(t, unLock)
-	client.UnLock(lockKey)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	var locked bool
+	go func() {
+		defer wg.Done()
+		locked = client.Lock(lockKey, 10*time.Second)
+		time.Sleep(50 * time.Millisecond)
+		client.UnLock(lockKey)
+	}()
+	go func() {
+		defer wg.Done()
+		time.Sleep(10 * time.Millisecond)
+		locked = client.Lock(lockKey, 10*time.Second)
+		client.UnLock(lockKey)
+	}()
+	wg.Wait()
+	assert.False(t, locked)
 }
